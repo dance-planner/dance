6c1bc9e38d6e0d6bcf534e507c9e7135c8a2a375
Michael Spengler <mycamper@gmx.de>
2020-08-13 01:13:50 +0200

    express server

--- server/express-server.ts
+++ server/express-server.ts
@@ -4,49 +4,119 @@ import * as express from 'express'
 import * as compression from 'compression'
 const http = require('http')
 const https = require('https')
+const cors = require('cors')
+const shell = require('shelljs');
 
-export const config = fs.readJSON(path.join(path.resolve(''), './.env.json'))
+export const config = fs.readJSON(path.join(path.resolve(''), './../topsecret/.env.json'))
 
-const app = express()
-const pathToStaticAssets = path.join(path.resolve(''), './../server/docs')
-app.use(express.static(pathToStaticAssets))
+executeMasterplan()
 
-app.use(compression())
+async function executeMasterplan() {
+  // Masterplan
+  regularlyGetTheLatestFancyShit()
+  const app = express();
+  app.use(cors())
+  app.use(compression())
+  const mainStaticAssetsPath = useStaticAssets(app)
+  const html = await readPageToMainMemory(mainStaticAssetsPath)
+  defineRoutes(app, html)
+  ensureRedirectingFromUnsafeHostToSaveHost()
 
-const html = fs.read(`${pathToStaticAssets}/i-want-compression-via-route.html`)
+  startListening(app)
 
-app.get('/', (req, res) => {
-  res.send(html)
-})
-
-if (config.httpsPort > 0) {
-  const privateKey = fs.read(config.certificatePrivateKeyFile)
-  const certificate = fs.read(config.certificateFile)
-  const credentials = { key: privateKey, cert: certificate }
-  const httpsServer = https.createServer(credentials, app)
-  httpsServer.listen(config.httpsPort)
 }
 
-if (config.httpPort > 0) {
-  const httpServer = http.createServer(app)
-  httpServer.listen(config.httpPort)
-  console.log(`listening on : http://localhost:${config.httpPort}`)
+
+
+// Details
+async function readPageToMainMemory(pathToStaticAssets: string): Promise<string> {
+  return fs.read(`${pathToStaticAssets}/i-want-compression-via-route.html`)
 }
 
-let forwarded = false
-ensureRedirectingFromUnsafeHostToSaveHost()
+function regularlyGetTheLatestFancyShit() {
+  setInterval(async () => {
+    const commandToBeExecuted = `./topsecret/pull.sh`
+    // const commandToBeExecuted = `ls`
+    try {
+      shell.exec(commandToBeExecuted)
+    } catch (error) {
+      console.log(error.message)
+    }
+
+  }, 2 * 60 * 1000)
+}
+
+function useStaticAssets(app): string {
+  const pathToStaticAssets = path.join(path.resolve(''), './../server/docs')
+  app.use(express.static(pathToStaticAssets))
+  return pathToStaticAssets
+}
+
+
+function defineRoutes(app, html) {
+
+  let pathToEvents = path.join(path.resolve(''), './../server/docs')
+  app.get('/', (req, res) => {
+    res.send(html)
+  })
+
+  app.get('/images/getEventImage/name/:name', (req: any, res: any) => {
+    res.sendFile(`${pathToEvents}/events/${req.params.name}`);
+  });
+
+  app.get('/events/getAllEvents/key/:key', async (req: any, res: any) => {
+    res.send(fs.readJSON(`${pathToEvents}/events/events.json`));
+  });
+
+  app.get('/cities/getCitiesWithMin/minNumberOfInhabitants/:minNumberOfInhabitants/key/:key', async (req: any, res: any) => {
+    console.log(req.headers)
+    // const cities = CityService.getCitiesByPopulation(Number(req.params.minNumberOfInhabitants))
+    const cities = []
+    res.send(cities);
+  });
+
+  app.get('/location/getIPLocation/key/:key', async (req: any, res: any) => {
+    res.send({
+      name: 'Heidelberg',
+      lat: 49.40768,
+      lon: 8.69079,
+    });
+  });
+
+  app.get('/community/getTelegramGroups/key/:key', async (req: any, res: any) => {
+    res.send(fs.readJSON(`${pathToEvents}/groups/telegram.json`));
+  });
+}
+
+function startListening(app) {
+
+  if (config.httpsPort > 0) {
+    const privateKey = fs.read(config.certificatePrivateKeyFile)
+    const certificate = fs.read(config.certificateFile)
+    const credentials = { key: privateKey, cert: certificate }
+    const httpsServer = https.createServer(credentials, app)
+    httpsServer.listen(config.httpsPort)
+  }
+
+  if (config.httpPort > 0) {
+    const httpServer = http.createServer(app)
+    httpServer.listen(config.httpPort)
+    console.log(`listening on : http://localhost:${config.httpPort}`)
+  }
+}
 
 // tslint:disable-next-line: only-arrow-functions
 function ensureRedirectingFromUnsafeHostToSaveHost() {
   const unsafePort = 80
   const httpForwarderAPPListeningOnUnsafePort = express()
-
+  
+  let forwarded = false
   httpForwarderAPPListeningOnUnsafePort.get('*', (req, res) => {
     if (!forwarded) {
       forwarded = true
       setTimeout(() => {
         forwarded = false
-      },         100)
+      }, 100)
       res.send(`<!DOCTYPE html><html><head><meta http-equiv="refresh" content="0; url='https://dance-planner.org'" /></head><body><p>Redirecting to https: <a href="https://dance-planner.org">https://dance-planner.org/</a></p></body></html>`)
     }
   })
